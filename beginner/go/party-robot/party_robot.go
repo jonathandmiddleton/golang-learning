c9c550e228f1eec32108902078ad7df492928cb4
Jonathan Middleton <jonathanmiddleton@Jonathans-MacBook-Pro.local>
2022-02-03 22:20:48 +0200

    iteration 2

--- beginner/go/party-robot/party_robot.go
+++ beginner/go/party-robot/party_robot.go
@@ -13,6 +13,6 @@ func HappyBirthday(name string, age int) string {
 }
 
 // AssignTable assigns a table to each guest.
-func AssignTable(name string, table int, seatmate string, direction string, distance float64) string {
-	return fmt.Sprintf("Welcome to my Party, %s!\nYou have been assigned to table %03d. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, seatmate)
+func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
+	return fmt.Sprintf("Welcome to my party, %s!\nYou have been assigned to table %03d. Your table is %v, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, neighbor)
 }
