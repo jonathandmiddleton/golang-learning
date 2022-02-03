83d4dff9c8aa7c71cd0f1df4b664d8dce9eb92d1
Jonathan Middleton <jonathanmiddleton@Jonathans-MacBook-Pro.local>
2022-02-03 12:26:16 +0200

    tech palace

--- beginner/go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ beginner/go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,22 @@
 package techpalace
 
+import (
+	"strconv"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	return ""
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	return ""
-}
+	stars := strings.Repeat("*", numStarsPerLine)
+	return stars + "\n" + welcomeMsg + "\n" + stars
+
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	return ""
+		return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
 }
